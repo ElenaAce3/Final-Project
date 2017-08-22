1193467fa59e5ce81c1d8b51040a442a6b0ac97e
Twitter <twitter@Twitters-MacBook-Pro.local>
2017-08-22 09:44:58 -0700

    undid all additions in settings/databases

--- projects/helloapp/helloapp/settings.py
+++ projects/helloapp/helloapp/settings.py
@@ -77,21 +77,20 @@ WSGI_APPLICATION = 'helloapp.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'schoolclubs$default',
-        'USER': 'schoolclubs',
-        'PASSWORD': 'findclubs@skool',
-        'HOST': 'schoolclubs.mysql.pythonanywhere-services.com',
-        'TEST': {
-          'NAME': 'schoolclubs$test_schoolclubs$default',
-          }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'schoolclubs$default',
+    #     'USER': 'schoolclubs',
+    #     'PASSWORD': 'findclubs@skool',
+    #     'HOST': 'schoolclubs.mysql.pythonanywhere-services.com',
+    #     'TEST': {
+    #       'NAME': 'schoolclubs$test_schoolclubs$default',
+    #       }
+    }
 
 
 # Password validation
